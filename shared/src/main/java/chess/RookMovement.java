03c29dd2bd5687617a71fb100ac4c643b0bc4785
pdporterman <pdporter@byu.edu>
2024-01-12 10:59:30 -0700

    fix(tests) removed extra credit tests

--- shared/src/main/java/chess/RookMovement.java
+++ shared/src/main/java/chess/RookMovement.java
@@ -1,11 +1,27 @@
 package chess;
-
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class RookMovement {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> moves = new ArrayList<>(); //use a hash set
         int row = myPosition.getRow();
-        while (myPosition){}
-    }
+        int col = myPosition.getColumn();
+        for (int i = row + 1; i <= 7; i++){
+            if (board.getPiece(myPosition) != null){if ();};
+            moves.add(new ChessMove(myPosition, new ChessPosition(i, col),null));
+        };
+        for (int i = row - 1; i >= 0; i--){
+            if (board.getPiece(myPosition) != null){break;};
+            moves.add(new ChessMove(myPosition, new ChessPosition(i, col),null));
+        }
+        for (int i = col + 1; i <= 7; i++){
+            if (board.getPiece(myPosition) != null){break;};
+            moves.add(new ChessMove(myPosition, new ChessPosition(row, i),null));
+        };
+        for (int i = col - 1; i >= 0; i--){
+            if (board.getPiece(myPosition) != null){break;};
+            moves.add(new ChessMove(myPosition, new ChessPosition(row, i),null));
 
+    }
 }
