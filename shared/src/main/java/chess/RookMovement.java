6aa6373d00528f3de5fa16d5ec1d6701fbffbb79
pdporterman <pdporter@byu.edu>
2024-01-13 15:44:29 -0700

    Update RookMovement.java

--- shared/src/main/java/chess/RookMovement.java
+++ shared/src/main/java/chess/RookMovement.java
@@ -1,14 +1,54 @@
 package chess;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class RookMovement {
+
+
+    private void getRows(ChessBoard board, ChessPosition myPosition, ChessPiece current, int row, int col, int direction) {
+        int border = (direction > 0) ? 7 : 0;
+        for (int i = row + direction; i-direction != border; i += direction) {
+            ChessPosition next = new ChessPosition(i, col);
+            if (board.getPiece(next) != null) {
+                if (board.getPiece(next).getTeamColor() != current.getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, next, null));
+                }
+                break;
+            }
+            else{
+                moves.add(new ChessMove(myPosition, next, null));
+            }
+        }
+    }
+
+    private void getCols(ChessBoard board, ChessPosition myPosition, ChessPiece current, int row, int col, int direction) {
+        int border = (direction > 0) ? 7 : 0;
+        for (int i = col + direction; i-direction != border; i += direction) {
+            ChessPosition next = new ChessPosition(row, i);
+            if (board.getPiece(next) != null) {
+                if (board.getPiece(next).getTeamColor() != current.getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, next, null));
+                }
+                break;
+            }
+            else{
+                moves.add(new ChessMove(myPosition, next, null));
+            }
+        }
+    }
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves = new ArrayList<>(); //use a hash set
+        Collection<ChessMove> moves = new HashSet<>(); //use a hash set
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
         ChessPiece current = board.getPiece(myPosition);
+        getRows(board, myPosition, current, row, col, 1); //get greater rows
+        getRows(board, myPosition, current, row, col, -1); //get lesser rows
+        getCols(board, myPosition, current, row, col, 1); // get greater cols
+        getCols(board, myPosition, current, row, col, -1); //get lesser cols
+        return moves;
+        /*
         for (int i = row + 1; i <= 7; i++) {
             ChessPosition next = new ChessPosition(i, col);
             if (board.getPiece(next) != null) {
@@ -23,7 +63,8 @@ public class RookMovement {
         for (int i = row - 1; i >= 0; i--) {
             if (board.getPiece(myPosition) != null) {
                 break;
-            };
+            }
+            ;
             moves.add(new ChessMove(myPosition, new ChessPosition(i, col), null));
         }
         for (int i = col + 1; i <= 7; i++) {
@@ -42,4 +83,6 @@ public class RookMovement {
             moves.add(new ChessMove(myPosition, new ChessPosition(row, i), null));
 
         }
+        */
     }
+}
