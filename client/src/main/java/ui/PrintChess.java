0ceb2b543f374ee108a2d99bf5863cf8d2e5844d
pdporterman <pdporter@byu.edu>
2024-03-14 09:45:13 -0600

    Update PrintChess.java

--- client/src/main/java/ui/PrintChess.java
+++ client/src/main/java/ui/PrintChess.java
@@ -1,10 +1,10 @@
 package ui;
 
 import chess.ChessGame;
+import chess.ChessPiece;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 import static ui.EscapeSequences.*;
 
@@ -15,28 +15,62 @@ public class PrintChess {
     private static final int LINE_WIDTH_IN_CHARS = 1;
 
 
-    public static void main(){
-//        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-//        ChessGame game = new ChessGame();
-//        ArrayList<String> header = new String[{"a", "b", "c", "d", "e", "f", "g", "h"}];
-//        out.print(ERASE_SCREEN);
-//
-//
-//        drawBoard(, );
+    public static void main(String[] args){
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        ChessPiece[][] board = new ChessGame().getBoard().getBoard();
+        String[] header = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        String[] margin = {"1","2","3","4","5","6","7","8"};
+        out.print(ERASE_SCREEN);
+
+
+        drawBoard(out, header, margin, board);
 
 
     }
 
-    public static void drawBoard(ArrayList<String> headers, ArrayList<String> margin, ChessGame game){
-        drawHead(headers);
-        drawRow(margin, game);
-        drawHead(headers);
+    public static void drawBoard(PrintStream out, String[] headers, String[] margin, ChessPiece[][] board){
+        drawHeaders(out, headers);
+        drawRows(out, margin, board);
+        drawHeaders(out, headers);
     }
 
-    private static void drawRow(ArrayList<String> margin, ChessGame game) {
+    private static void drawRows(PrintStream out, String[] margin, ChessPiece[][] board) {
     }
 
-    private static void drawHead(ArrayList<String> headers) {
+    private static void drawHeaders(PrintStream out, String[] headers) {
+        setBlack(out);
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeader(out, headers[boardCol]);
+
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
+            }
+        }
+
+
+    }
+
+    private static void drawHeader(PrintStream out, String header) {
+        int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
+        int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
+
+        out.print(EMPTY.repeat(prefixLength));
+        printHeaderText(out, header);
+        out.print(EMPTY.repeat(suffixLength));
+    }
+
+    private static void printHeaderText(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+
+        out.print(player);
+
+        setBlack(out);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
     }
 
 
