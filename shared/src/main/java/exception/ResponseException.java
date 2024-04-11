7a86bb732b1ad28e69ef239a231dd0330e45c1a6
pdporterman <pdporter@byu.edu>
2024-04-11 13:39:25 -0600

    Update ResponseException.java

--- shared/src/main/java/exception/ResponseException.java
+++ shared/src/main/java/exception/ResponseException.java
@@ -2,7 +2,6 @@ package exception;
 
 public class ResponseException extends Exception {
     final private int statusCode;
-
     public ResponseException(int statusCode, String message) {
         super(message);
         this.statusCode = statusCode;
